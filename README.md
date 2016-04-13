6a590164b512e3d0f2953f81bf82b3004953ba21
Surendra <persaudsurendra@gmail.com>
2016-04-13 13:27:22 -0400

    edited website entirely

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # surenzxx.github.io
 My awesome webite
+
+creds to mamadou: sadio.me
---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*
+    This website was inspired by: sadio.me
+*/
+//The Hard Code
+
+//When user clicks Please say hi
+//the popup textbox showsup
+
+//Connects to Google Analytics
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-73415098-1', 'auto');
+  ga('send', 'pageview');
+
+</script>`
